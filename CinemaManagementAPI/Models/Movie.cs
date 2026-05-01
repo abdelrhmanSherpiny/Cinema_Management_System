91026358564102a64957e3a9951640145b6e2e70
abdelrhmanSherpiny <abdelrahmanelshirpeny@gmail.com>
2026-05-01 04:14:46 +0300

    Models added

--- CinemaManagementAPI/Models/Movie.cs
+++ CinemaManagementAPI/Models/Movie.cs
@@ -7,5 +7,25 @@ namespace CinemaManagementAPI.Models
         public int Release_Year { get; set; }
         public TimeSpan Duration { get; set; }
         public string Country { get; set; } = string.Empty;
+
+        public int Genre_ID { get; set; }
+
+        public List<Actor> Actors { get; set; } = new List<Actor>();
+    }
+
+    public class Actor
+    {
+        public int Actor_ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Birth_Date { get; set; }
+        public string Nationality { get; set; } = string.Empty;
+    }
+
+    public class Genre
+    {
+        public int Genre_ID { get; set; }
+        public string Name { get; set; } = string.Empty;
     }
 }
+
+
